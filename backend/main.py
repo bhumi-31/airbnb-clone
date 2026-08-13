9bd856cff0927d6a7bf0c5fc2f28827458317c1c
Bhumika Narula <146376815+bhumi-31@users.noreply.github.com>
2026-08-14 02:59:29 +0530

    Seed production listings and update CORS

--- backend/main.py
+++ backend/main.py
@@ -8,18 +8,20 @@ from models.user import User
 from routers.listings import router as listing_router
 from routers.bookings import router as booking_router
 from routers.host import router as host_router
+from seed import seed_database
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "https://airbnb-clone-amber-three.vercel.app",],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
 Base.metadata.create_all(bind=engine)
+seed_database()
 
 app.include_router(listing_router)
 app.include_router(booking_router)
